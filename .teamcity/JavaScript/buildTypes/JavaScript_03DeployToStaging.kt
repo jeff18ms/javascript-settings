250dd5d35dd9bb5d9035a32967375d497691a24b
jeff18ms <jeffrey.d.vasquez@gmail.com>
2016-11-22 05:36:03 +0000

    Removed IE tests

--- .teamcity/JavaScript/buildTypes/JavaScript_03DeployToStaging.kt
+++ .teamcity/JavaScript/buildTypes/JavaScript_03DeployToStaging.kt
@@ -22,10 +22,7 @@ object JavaScript_03DeployToStaging : BuildType({
             snapshot {
             }
         }
-        dependency(JavaScript.buildTypes.JavaScript_02IE) {
-            snapshot {
-            }
-        }
+
     }
     triggers {
         vcs {
